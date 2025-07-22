445696b4bb9dac7a08aebb2fb8671816213fc03e
kim foss <fosskm@msn.com>
2025-07-22 10:51:49 -0400

    users GET endpoint working

--- back-end/routes/users/users.js
+++ back-end/routes/users/users.js
@@ -1,9 +1,29 @@
 import express from "express";
+import { client, connect } from './db.js';
+
+const dbName = 'usersdb';
+const collectionName = 'users';
+
+connect();  // Connect to MongoDB as soon as this file is loaded
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.send("GET called");    
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+
+    // Only return username and email fields, exclude _id and password
+    const prj = { user: 1, email: 1, _id: 0 };
+
+    const users = await collection.find({}).project(prj).toArray();
+
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
+
 
 router.post('/', async (req, res) => {
     res.send("POST called");  
