d61acba7d8ef1f3773ee828768a7fde748cf8d0f
Scarfo, Stephen <Stephen.Scarfo@LibertyMutual.com>
2025-07-22 14:24:21 -0400

    SavedQueries are now retained after refresh

--- front-end/src/NewsReader.js
+++ front-end/src/NewsReader.js
@@ -1,15 +1,16 @@
-import { QueryForm } from './QueryForm';
-import { Articles } from './Articles';
-import { useState, useEffect } from 'react';
-import { exampleQuery, exampleData } from './data';
-import { SavedQueries } from './SavedQueries';
+import { QueryForm } from "./QueryForm";
+import { Articles } from "./Articles";
+import { useState, useEffect } from "react";
+import { exampleQuery, exampleData } from "./data";
+import { SavedQueries } from "./SavedQueries";
 
 export function NewsReader() {
   const [query, setQuery] = useState(exampleQuery); // latest query sent to newsapi
-  const [data, setData] = useState(exampleData);   // current data returned from newsapi
+  const [data, setData] = useState(exampleData); // current data returned from newsapi
   const [queryFormObject, setQueryFormObject] = useState({ ...exampleQuery });
 
   const urlNews = "/news";
+  const urlQueries = "/queries";
 
   const [savedQueries, setSavedQueries] = useState([{ ...exampleQuery }]);
 
@@ -17,7 +18,39 @@ export function NewsReader() {
     getNews(query);
   }, [query]);
 
- // Add onSavedQuerySelect here
+  useEffect(() => {
+    getQueryList();
+  }, []);
+
+  async function getQueryList() {
+    try {
+      const response = await fetch(urlQueries);
+      if (response.ok) {
+        const data = await response.json();
+        console.log("savedQueries has been retrieved: ");
+        setSavedQueries(data);
+      }
+    } catch (error) {
+      console.error("Error fetching news:", error);
+    }
+  }
+  async function saveQueryList(savedQueries) {
+    try {
+      const response = await fetch(urlQueries, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(savedQueries),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      console.log("savedQueries array has been persisted:");
+    } catch (error) {
+      console.error("Error fetching news:", error);
+    }
+  }
+
+  // Add onSavedQuerySelect here
   function onSavedQuerySelect(selectedQuery) {
     setQueryFormObject(selectedQuery);
     setQuery(selectedQuery);
@@ -26,14 +59,13 @@ export function NewsReader() {
   function onFormSubmit(queryObject) {
     let newSavedQueries = [];
     newSavedQueries.push(queryObject);
-
     for (let query of savedQueries) {
       if (query.queryName !== queryObject.queryName) {
         newSavedQueries.push(query);
       }
     }
-
     console.log(JSON.stringify(newSavedQueries));
+    saveQueryList(newSavedQueries);
     setSavedQueries(newSavedQueries);
     setQuery(queryObject);
   }
@@ -66,32 +98,31 @@ export function NewsReader() {
   }
 
   return (
-  <div>
     <div>
-      <section className="parent">
-        <div className="box">
-          <span className='title'>Query Form</span>
-          <QueryForm
-            setFormObject={setQueryFormObject}
-            formObject={queryFormObject}
-            submitToParent={onFormSubmit}
-          />
-        </div>
-        <div className="box">
-          <span className='title'>Saved Queries</span>
-          <SavedQueries
-            savedQueries={savedQueries}
-            selectedQueryName={query.queryName}
-            onQuerySelect={onSavedQuerySelect}
-          />
-        </div>
-        <div className="box">
-          <span className='title'>Articles List</span>
-          <Articles query={query} data={data} />
-        </div>
-      </section>
+      <div>
+        <section className="parent">
+          <div className="box">
+            <span className="title">Query Form</span>
+            <QueryForm
+              setFormObject={setQueryFormObject}
+              formObject={queryFormObject}
+              submitToParent={onFormSubmit}
+            />
+          </div>
+          <div className="box">
+            <span className="title">Saved Queries</span>
+            <SavedQueries
+              savedQueries={savedQueries}
+              selectedQueryName={query.queryName}
+              onQuerySelect={onSavedQuerySelect}
+            />
+          </div>
+          <div className="box">
+            <span className="title">Articles List</span>
+            <Articles query={query} data={data} />
+          </div>
+        </section>
+      </div>
     </div>
-  </div>
-);
-
+  );
 }
